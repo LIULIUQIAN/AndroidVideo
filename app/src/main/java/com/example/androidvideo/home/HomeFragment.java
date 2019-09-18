76eabf8a21837dbceaa7b4691720f7124f6e0ade
刘六乾 <liuliuqian@yanxiu.com>
2019-09-18 14:19:33 +0800

    首页开发

--- app/src/main/java/com/example/androidvideo/home/HomeFragment.java
+++ app/src/main/java/com/example/androidvideo/home/HomeFragment.java
@@ -6,14 +6,21 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.androidvideo.R;
 import com.example.androidvideo.base.BaseFragment;
+import com.example.androidvideo.model.Channel;
 import com.hejunlin.superindicatorlibray.CircleIndicator;
 import com.hejunlin.superindicatorlibray.LoopViewPager;
 
 
 public class HomeFragment extends BaseFragment {
+
+    private GridView mGridView;
 
     @Override
     protected int getLayoutId() {
@@ -29,10 +36,58 @@ public class HomeFragment extends BaseFragment {
         viewPager.setLooperPic(true);
         indicator.setViewPager(viewPager);
 
+        mGridView = bindViewId(R.id.gv_channel);
+        mGridView.setAdapter(new ChannelAdapter());
+
     }
 
     @Override
     protected void initData() {
 
     }
+
+    class ChannelAdapter extends BaseAdapter{
+
+        @Override
+        public int getCount() {
+            return 9;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return new Channel(i+1,getActivity());
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return i;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            Channel channel = (Channel) getItem(i);
+            ViewHolder holder =null;
+            if (view == null){
+                view = LayoutInflater.from(getActivity()).inflate(R.layout.home_grid_item,null);
+                holder = new ViewHolder();
+                holder.imageView = view.findViewById(R.id.iv_home_item_img);
+                holder.textView = view.findViewById(R.id.tv_home_item_text);
+                view.setTag(holder);
+
+            }else {
+                holder = (ViewHolder) view.getTag();
+
+            }
+            holder.textView.setText(channel.getChannelName());
+            holder.imageView.setImageResource(channel.getImagResId());
+
+            return view;
+        }
+    }
+
+    class ViewHolder{
+        TextView textView;
+        ImageView imageView;
+    }
+
 }
