6c13fd15cd8012d2a04737c91853d4e3b85e2ecd
刘六乾 <liuliuqian@yanxiu.com>
2019-09-18 11:42:39 +0800

    播客开发

--- app/src/main/java/com/example/androidvideo/home/BlogFragment.java
+++ app/src/main/java/com/example/androidvideo/home/BlogFragment.java
@@ -1,16 +1,19 @@
 package com.example.androidvideo.home;
 
-import android.content.Context;
-import android.net.Uri;
-import android.os.Bundle;
-import android.view.LayoutInflater;
+
 import android.view.View;
-import android.view.ViewGroup;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.widget.ProgressBar;
 
 import com.example.androidvideo.R;
 import com.example.androidvideo.base.BaseFragment;
 
 public class BlogFragment extends BaseFragment {
+
+    private WebView mWebView;
+    private ProgressBar mProgressBar;
 
     @Override
     protected int getLayoutId() {
@@ -20,10 +23,31 @@ public class BlogFragment extends BaseFragment {
     @Override
     protected void initView() {
 
+        mWebView = bindViewId(R.id.webview);
+        mProgressBar = bindViewId(R.id.pb_progress);
+
+        WebSettings settings = mWebView.getSettings();
+        settings.setBuiltInZoomControls(true);
+        settings.setJavaScriptEnabled(true);
+        mWebView.loadUrl("https://www.baidu.com/");
+        mWebView.setWebChromeClient(mWebChromeClient);
+
     }
 
     @Override
     protected void initData() {
 
     }
+
+    private WebChromeClient mWebChromeClient = new WebChromeClient(){
+
+        @Override
+        public void onProgressChanged(WebView view, int newProgress) {
+            mProgressBar.setProgress(newProgress);
+            if (newProgress == 100){
+                mProgressBar.setVisibility(View.GONE);
+            }
+            super.onProgressChanged(view, newProgress);
+        }
+    };
 }
