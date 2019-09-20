6016e3eed08ba7e00127053af7b9c5499ed7e1eb
3284904004@qq.com <FFzf2016>
2019-09-20 22:28:10 +0800

    剧集开发

--- app/src/main/java/com/example/androidvideo/detail/AlbumPlayGridFragment.java
+++ app/src/main/java/com/example/androidvideo/detail/AlbumPlayGridFragment.java
@@ -2,6 +2,10 @@ package com.example.androidvideo.detail;
 
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.SystemClock;
+import android.widget.AbsListView;
 import android.widget.GridView;
 
 import androidx.annotation.Nullable;
@@ -12,6 +16,7 @@ import com.example.androidvideo.api.SiteApi;
 import com.example.androidvideo.base.BaseFragment;
 import com.example.androidvideo.model.Album;
 import com.example.androidvideo.model.ErrorInfo;
+import com.example.androidvideo.model.sohu.Video;
 import com.example.androidvideo.model.sohu.VideoList;
 
 public class AlbumPlayGridFragment extends BaseFragment {
@@ -25,6 +30,12 @@ public class AlbumPlayGridFragment extends BaseFragment {
     private boolean mIsShowDesc;
     private int mPageNo;
     private int mPageSize;
+    private VideoItemAdapter mVideoItemAdapter;
+    private int mPageTotal;
+    private VideoList mVideoList = new VideoList();
+    private Handler mHandler = new Handler(Looper.getMainLooper());
+    private boolean mIsFristSelection = true;
+    private boolean isLoading = false;
 
     public static AlbumPlayGridFragment newInstance(Album album, boolean isShowDesc, int initVideoPosition){
         AlbumPlayGridFragment fragment = new AlbumPlayGridFragment();
@@ -51,6 +62,28 @@ public class AlbumPlayGridFragment extends BaseFragment {
             mPageNo = 0;
             mPageSize = 50;
         }
+        mGridView.setNumColumns(mIsShowDesc ? 1 : 6);
+        mVideoItemAdapter = new VideoItemAdapter(getActivity(),mAlbum.getVideoTotal(),mIsShowDesc,mVideoSelectedListner);
+        mGridView.setAdapter(mVideoItemAdapter);
+        mPageTotal = (mAlbum.getVideoTotal() + mPageSize -1)/ mPageSize;
+
+        mGridView.setOnScrollListener(new AbsListView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(AbsListView absListView, int i) {
+
+            }
+
+            @Override
+            public void onScroll(AbsListView absListView, int i, int i1, int i2) {
+                if (i2 > 0){
+                    int lastViewVisible = i + i1;
+                    if (lastViewVisible == i2 && !isLoading){
+                        loadData();
+                    }
+
+                }
+            }
+        });
     }
 
     @Override
@@ -62,17 +95,46 @@ public class AlbumPlayGridFragment extends BaseFragment {
 
     private void loadData(){
         mPageNo++;
+        isLoading = true;
         SiteApi.onGetVideo(mPageSize, mPageNo, mAlbum, new OnGetVideoListener() {
             @Override
             public void OnGetVideoSuccess(VideoList videoList) {
-
+                isLoading = false;
+                mVideoList.addAll(videoList);
+                mVideoItemAdapter.setmVideoList(mVideoList);
+                mHandler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (mVideoItemAdapter != null){
+                            mVideoItemAdapter.notifyDataSetChanged();
+                        }
+                        if (mVideoItemAdapter.getCount() > mInitPosition && mIsFristSelection){
+                            mGridView.setSelection(mInitPosition);
+                            mGridView.setItemChecked(mInitPosition,true);
+                            mIsFristSelection = false;
+                            SystemClock.sleep(100);
+                            mGridView.smoothScrollToPosition(mInitPosition);
+                        }
+                    }
+                });
             }
 
             @Override
             public void OnGetVideoFailed(ErrorInfo info) {
-
+                isLoading = false;
             }
         });
     }
+
+    private OnVideoSelectedListener mVideoSelectedListner = new OnVideoSelectedListener() {
+        @Override
+        public void onVideoSelected(Video video, int position) {
+            if (mGridView != null){
+                mGridView.setSelection(position);
+                mGridView.setItemChecked(position,true);
+
+            }
+        }
+    };
 }
 
--- app/src/main/java/com/example/androidvideo/detail/VideoItemAdapter.java
+++ app/src/main/java/com/example/androidvideo/detail/VideoItemAdapter.java
@@ -1,11 +1,18 @@
 package com.example.androidvideo.detail;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.media.MediaPlayer;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
+import com.example.androidvideo.R;
+import com.example.androidvideo.model.sohu.Video;
 import com.example.androidvideo.model.sohu.VideoList;
 
 public class VideoItemAdapter extends BaseAdapter {
@@ -13,18 +20,22 @@ public class VideoItemAdapter extends BaseAdapter {
     private Context mContext;
     private int mTotalCount;
     private OnVideoSelectedListener mListener;
-    private VideoList mVideoList;
+    private VideoList mVideoList = new VideoList();
     private boolean mIsFirst = true;
+    private boolean mIsShowTitleContent;
+    private int currentSelectIndex = 0;
 
-    public VideoItemAdapter(Context context, int totalCount, OnVideoSelectedListener listener){
+    public VideoItemAdapter(Context context, int totalCount,boolean isShow, OnVideoSelectedListener listener){
         mContext = context;
         mTotalCount = totalCount;
         mListener = listener;
+        mIsShowTitleContent = isShow;
 
     }
 
     @Override
     public int getCount() {
+
         return mVideoList.size();
     }
 
@@ -39,8 +50,57 @@ public class VideoItemAdapter extends BaseAdapter {
     }
 
     @Override
-    public View getView(int i, View view, ViewGroup viewGroup) {
-        return null;
+    public View getView(final int i, View view, ViewGroup viewGroup) {
+        ViewHolder viewHolder;
+        if (view == null){
+            view = LayoutInflater.from(mContext).inflate(R.layout.video_item_layout,null);
+            viewHolder = new ViewHolder();
+            viewHolder.videoContainer = view.findViewById(R.id.video_container);
+            viewHolder.videoTitle = view.findViewById(R.id.bt_video_title);
+            view.setTag(viewHolder);
+
+        }else {
+            viewHolder = (ViewHolder) view.getTag();
+        }
+        if (currentSelectIndex == i) {
+            viewHolder.videoContainer.setBackgroundResource(R.color.colorAccent);
+        } else {
+            viewHolder.videoContainer.setBackgroundColor(Color.TRANSPARENT);
+        }
+
+        final Video video = mVideoList.get(i);
+        if (mIsShowTitleContent){
+            if (TextUtils.isEmpty(video.getVideoName())){
+                viewHolder.videoTitle.setText(String.valueOf(i+1));
+            }else {
+                viewHolder.videoTitle.setText(video.getVideoName());
+            }
+        }else {
+            viewHolder.videoTitle.setText(String.valueOf(i+1));
+        }
+        if (i == 0 && mIsFirst){
+            mListener.onVideoSelected(video,i);
+            mIsFirst = false;
+        }
+        viewHolder.videoTitle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                currentSelectIndex = i;
+                mListener.onVideoSelected(video,i);
+            }
+        });
+
+        return view;
+    }
+
+    public void setmVideoList(VideoList mVideoList) {
+        this.mVideoList = mVideoList;
+    }
+
+
+    class ViewHolder{
+        LinearLayout videoContainer;
+        Button videoTitle;
     }
 
 
